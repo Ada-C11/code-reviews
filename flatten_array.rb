213014972a8d24e49d278735127125cbe1bed15f
Chris M <chris@adadevelopersacademy.org>
2019-08-15 10:23:47 -0700

    Update flatten_array.rb

--- flatten_array.rb
+++ flatten_array.rb
@@ -1,16 +1,28 @@
-class FlattenArray
+require 'pry'
 
-    def self.flatten(array)
-        retVal = []
-        for item in array
-            if item == nil
-                next
-            elsif item.kind_of?(Array)
-                retVal += flatten(item)
-            else 
-                retVal.push(item)
-            end
-        end
-        return retVal
+class FlattenArray
+  def self.flatten(array)
+    flattened_array = []
+
+    array.each do |element|
+      flattened_array = self.add_element_array(element, flattened_array)
     end
+    flattened_array.compact
+  end
+
+  def self.add_element_array(element, array)
+    # binding.pry
+    if element.class == Array
+      element.each do |ele|
+        array = self.add_element_array(ele, array)
+      end
+    else
+      array << element
+    end
+    array
+  end
+end
+
+module BookKeeping
+  VERSION = 1 # Where the version number matches the one in the test.
 end
